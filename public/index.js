b750df4b080d6c49bd31b96fdf4d1fff988b76b3
Muhammed Ajmal CC <muhammedajmalcc6424094@gmail.com>
2025-09-08 11:13:21 +0530

    bg video set

---
+++ public/index.js
@@ -0,0 +1,11 @@
+import MainBackground from "./images/7578552-uhd_3840_2160_30fps.mp4"
+
+import WallFrame1 from "./images/pexels-atbo-66986-245208.jpg" 
+import WallFrame2 from "./images/pexels-fotios-photos-1090638.jpg" 
+import WallFrame3 from "./images/pexels-kamo11235-667838.jpg" 
+
+
+
+
+
+export default {MainBackground, WallFrame1, WallFrame2, WallFrame3};
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Button from "../layouts/Button";
-
+import publicAssets from "../../public/index.js";
 const Home = () => {
   return (
-    <div className=" min-h-screen flex flex-col justify-center lg:px-32 px-5 text-white bg-[url('assets/img/home.png')] bg-no-repeat bg-cover opacity-90">
+    <div className="relative min-h-screen flex flex-col justify-center lg:px-32 px-5 text-white">
+      <div className="absolute inset-0 w-full h-full bg-black opacity-90 -z-20"></div>
+      <video
+        className="absolute inset-0 w-full h-full object-cover opacity-90 -z-10"
+        src={publicAssets.MainBackground}
+        autoPlay
+        loop
+        muted
+        playsInline
+      />
       <div className=" w-full lg:w-4/5 space-y-5 mt-10">
         <h1 className="text-5xl font-bold leading-tight">
           Empowering Health Choices for a Vibrant Life Your Trusted..
